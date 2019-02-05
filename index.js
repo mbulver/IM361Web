0fc93fb5822ed74c2f39d9beb0fb29c45e0607dc
mbulver <46090670+mbulver@users.noreply.github.com>
2019-02-05 15:03:28 -0600

    I don't know what I'm doing
    I'm so lost

---
+++ index.js
@@ -0,0 +1,11 @@
+//so I couldn't figure out how to run node js through that console but yeah enjoy this tip calculator I found from: https://repl.it/@Lehron/Tip-calculator-using-JavaScript-and-console
+var amount = prompt("Enter amount of bill");
+var bill = amount;
+var tip = function() {
+  var total = function() {
+  var tipCalc = bill * .10;
+    console.log(tipCalc);
+  }
+total();
+}
+tip();
