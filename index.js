459456cd9719d339390cd75f59c037676b8e0279
Max Bulver <46090670+mbulver@users.noreply.github.com>
2019-02-05 15:04:50 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// So this is a tip calculator I found, which I then ran in the console on Google Chrome because the gitbash I had open in class closed
 //so I couldn't figure out how to run node js through that console but yeah enjoy this tip calculator I found from: https://repl.it/@Lehron/Tip-calculator-using-JavaScript-and-console
 var amount = prompt("Enter amount of bill");
 var bill = amount;
