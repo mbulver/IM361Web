9c24bc39fd785f301f6b6ed0dc52d56c38906d9f
mbulver <46090670+mbulver@users.noreply.github.com>
2019-02-09 18:05:47 -0600

    WebApp Update
    Added more custom fields to index,js and made a very rough Reminji Selector

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import Vue from 'vue/dist/vue.js'
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    message: 'Welcome to Planmo',
+    tasks: 0,
+    completed_tasks: 0,
+    reminjis_used: 0,
+    user: 'Welcome, User'
+
   }
 });
 
