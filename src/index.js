9f7ba35af810fd063f05903faad31ee86c03defc
mbulver <46090670+mbulver@users.noreply.github.com>
2019-03-03 16:10:58 -0600

    Final
    Cut out all extra stuff that didn't need to be there. Added a few more style elements. Commented it all out. Pray4me

--- src/index.js
+++ src/index.js
@@ -4,11 +4,7 @@ import Vue from 'vue/dist/vue.js'
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Welcome to Planmo',
-    tasks: 0,
-    completed_tasks: 0,
-    reminjis_used: 0,
-    user: 'Welcome, User',
+    // defines all the pieces of data used in the webapp
     pageName: "Home",
     reminjis: ["Bunnles", "Zorb", "Storby", "Cubilee", "Hexapus"],
     selectedreminji: "Bunnles",
@@ -22,18 +18,22 @@ var app = new Vue({
     currentTodo: ""
   },
   computed:{
+    // defines the reminji view function which is used to view the current reminji during schedule creation
     reminjiview: function ()
     {
       return "Reminjis_" + this.selectedreminji + this.selectedcolor + ".png";
     },
+    //functino that gets the number of schedules
     numberOfSchedules: function() {
       return this.schedules.length;
     },
+    //function that gets if there are schedules
     hasSchedules: function() {
       return this.schedules.length > 0;
     }
   },
   methods:{
+    //function that creates the schedule after collecting data
   createSchedule: function(reminjiview, currentName, selectedcolor,) {
     this.schedules.push({
       reminji: reminjiview,
@@ -42,9 +42,11 @@ var app = new Vue({
     });
     this.pageName = 'Home';
   },
+  //allows users to add to dos to their schedules
   addTodo: function(schedule, todo) {
     schedule.todos.push(todo);
   },
+  //allows users to remove to dos to their schedules
   removeTodo: function(schedule, todo) {
     var index = schedule.todos.indexOf(todo);
 
